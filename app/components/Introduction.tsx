278cfbb2704dec2790e421dfaa732b8d53ddb075
Luis Woldu <luiswoldu@gmail.com>
2026-07-04 01:01:31 -0400

    Clean up

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -3,7 +3,7 @@ const Introduction = () => (
     <p className="text-lg sm:text-xl leading-relaxed">
       I'm building <a href="https://handsforu.com" target="_blank" rel="noopener noreferrer" className="underline hover:opacity-70 transition-opacity">Hands</a>, your AI personal chef.
       <br /><br />
-      This website is a creative outlet of my love for design, art, engineering, filmmaking, and writing.
+      This website is a  creative outlet of my love for design, art, engineering, filmmaking, Ai and writing.
       <br /><br />
       Connect with me on instagram, x, linkedin, or at luiswoldu at gmail dot com!
     </p>
